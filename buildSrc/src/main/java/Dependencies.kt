006653fd5815c0e828c5d87d1d59131ba72cce0f
KWY0218 <wodrk1001@gmail.com>
2022-07-28 09:23:15 +0900

    [chore] buildSrc 추가

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,27 @@
+object KotlinDependencies
+
+object AndroidXDependencies {
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+    const val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintLayoutVersion}"
+}
+
+object KtxDependencies {
+    const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtxVersion}"
+}
+
+object TestDependencies {
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val androidTest = "androidx.test.ext:junit:${Versions.androidTestVersion}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
+}
+
+object MaterialDependencies {
+    const val material = "com.google.android.material:material:${Versions.materialVersion}"
+}
+
+object KaptDependencies
+
+object ThirdPartyDependencies
+
+object ClassPathPlugins
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    const val coreKtxVersion = "1.8.0"
+    const val appCompatVersion = "1.4.2"
+    const val materialVersion = "1.6.1"
+    const val constraintLayoutVersion = "2.1.4"
+    const val jUnitVersion = "4.13.2"
+    const val androidTestVersion = "1.1.13"
+    const val espressoVersion = "3.4.0"
+}
